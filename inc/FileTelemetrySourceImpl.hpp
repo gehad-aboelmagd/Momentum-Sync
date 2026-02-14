d420141778875f3a7e3eb0ffbab5ee4f18eb0b0c
gehad-aboelmagd <jhad44734@gmail.com>
2026-02-14 15:40:05 +0200

    missing declarations

--- inc/FileTelemetrySourceImpl.hpp
+++ inc/FileTelemetrySourceImpl.hpp
@@ -7,6 +7,8 @@ class FileTelemetrySourceImpl: public ITelemetrySource
 {
 private:
 public:
+    bool openSource(std::string &path) override;
+    bool readSource(std::string &out) override;
 };
 
 #endif // FILE_TELEMETRY_SOURCE_IMPL_HPP_
