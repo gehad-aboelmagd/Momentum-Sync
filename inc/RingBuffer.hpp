04685a963016c877d8fba4f39282f0dfe912189f
gehad-aboelmagd <jhad44734@gmail.com>
2026-02-15 23:43:45 +0200

    implemented ring buffer

---
+++ inc/RingBuffer.hpp
@@ -0,0 +1,54 @@
+#ifndef RING_BUFFER_HPP_
+#define RING_BUFFER_HPP_
+
+#include <vector>
+#include <optional>
+
+template <typename T>
+class RingBuffer 
+{
+private:
+    std::vector<std::optional<T>> buffer;
+    int capacity; // overall
+    int counter; // current
+public:
+    RingBuffer(int size): capacity(size), counter{0}
+    {
+        buffer.resize(size);
+    }
+
+    RingBuffer(RingBuffer &other) = delete;
+        
+    RingBuffer(RingBuffer && other)
+    : buffer{std::move(other.buffer)}, capacity{other.capacity}, counter{other.counter}
+    {
+        other.capacity = other.counter = 0;
+    }
+    
+    RingBuffer & operator=(RingBuffer &other) = delete;
+
+    RingBuffer & operator=(RingBuffer &&other)
+    {
+        buffer = std::move(other.buffer);
+        capacity = other.capacity;
+        counter = other.counter;
+
+        other.capacity = other.counter = 0;
+
+        return *this;
+    }
+    
+    void tryPush(T value)
+    {
+        buffer[counter] = value;
+        counter = (counter + 1) % capacity;
+    }
+
+    std::optional<T> tryPop()
+    {
+        counter = (counter-1+capacity) % capacity;
+        return buffer[counter];
+    }
+};
+
+#endif // RING_BUFFER_HPP_
