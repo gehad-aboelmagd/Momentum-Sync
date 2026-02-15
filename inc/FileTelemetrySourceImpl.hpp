89ffe79ec8c2ec25f7a902de7c98a5126eb820b4
gehad-aboelmagd <jhad44734@gmail.com>
2026-02-15 11:52:15 +0200

    seperation of concerns, API, implementation

--- inc/FileTelemetrySourceImpl.hpp
+++ inc/FileTelemetrySourceImpl.hpp
@@ -1,11 +1,14 @@
 #ifndef FILE_TELEMETRY_SOURCE_IMPL_HPP_
 #define FILE_TELEMETRY_SOURCE_IMPL_HPP_
 
+#include <memory>
+#include "SafeFile.hpp"
 #include "ITelemetrySource.hpp"
 
 class FileTelemetrySourceImpl: public ITelemetrySource
 {
 private:
+    std::unique_ptr<SafeFile> file = nullptr;
 public:
     bool openSource(std::string &path) override;
     bool readSource(std::string &out) override;
--- src/FileTelemetrySourceImpl.cpp
+++ src/FileTelemetrySourceImpl.cpp
@@ -1,34 +1,27 @@
-#include <memory>
-#include "SafeFile.hpp"
-#include "ITelemetrySource.hpp"
+#include "FileTelemetrySourceImpl.hpp"
 
-class FileTelemetrySourceImpl: public ITelemetrySource
+
+bool FileTelemetrySourceImpl::openSource(std::string &path)
 {
-private:
-    std::unique_ptr<SafeFile> file = nullptr;
-public:
-    bool openSource(std::string &path) override
+    file.reset(new SafeFile(path));
+    if(file->getState() == -1)
     {
-        file.reset(new SafeFile(path));
-        if(file->getState() == -1)
-        {
-            return false;
-        }
-        return true;
-    }
-    bool readSource(std::string &out) override
-    {
-        if(file != nullptr && file->getState() != -1)
-        {
-            auto temp = file->readFile();
-            if(temp.has_value())
-            {
-                out = temp.value();
-                return true;
-            }
-            return false;
-        }
-        
         return false;
     }
-};
+    return true;
+}
+bool FileTelemetrySourceImpl::readSource(std::string &out)
+{
+    if(file != nullptr && file->getState() != -1)
+    {
+        auto temp = file->readFile();
+        if(temp.has_value())
+        {
+            out = temp.value();
+            return true;
+        }
+        return false;
+    }
+    
+    return false;
+}
