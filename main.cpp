459deb39af3b11e7386d0a31998bfc605ed7a635
gehad-aboelmagd <jhad44734@gmail.com>
2026-03-04 21:46:19 +0200

    integrating different project components

--- main.cpp
+++ main.cpp
@@ -1,16 +1,68 @@
 #include <iostream>
 #include "LogManager.hpp"
+#include "LogFormatter.hpp"
 #include "ConsoleSinkImpl.hpp"
 #include "FileSinkImpl.hpp"
+#include "FileTelemetrySourceImpl.hpp"
+#include "SomeIPTelemetrySourceImpl.hpp"
+
+class MyMomyntumSync
+{
+private:
+    std::string src_path = "cpu_readings.txt";
+    std::string logs_path = "logs.txt";
+    LogManager manager;
+    LogFormatter<CpuPolicy> formatter;
+    std::optional<LogMessage>msg;
+    std::string readings;
+    FileTelemetrySourceImpl src_file;
+    SomeIPTelemetrySourceImpl someip_src;
+    std::thread someip_thread;
+
+public:
+    MyMomyntumSync(): manager(5), src_file(src_path)
+    {
+        manager.addSink(std::unique_ptr<ConsoleSinkImpl>(new ConsoleSinkImpl(std::cout)));
+        manager.addSink(std::unique_ptr<FileSinkImpl>(new FileSinkImpl(logs_path)));
+
+        src_file.openSource();
+        someip_thread = std::thread([this]() {
+        someip_src.openSource();
+        });
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    void start()
+    {
+        for(int i=0; i<10; i++)
+        {
+            src_file.readSource(readings);   
+            msg = formatter.formatDataToLogMsg("app0", readings);
+            if(msg.has_value())
+            {
+                manager.writeToAll(msg.value());
+            }
+
+            someip_src.readSource(readings);
+            msg = formatter.formatDataToLogMsg("app0", readings);
+            if(msg.has_value())
+            {
+                manager.writeToAll(msg.value());
+            }
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        } 
+    }
+
+    ~MyMomyntumSync()
+    {
+        someip_thread.detach();
+    }
+};
 
 int main()
 {
-    LogManager manager;
-    manager.addSink(std::unique_ptr<ConsoleSinkImpl>(new ConsoleSinkImpl(std::cout)));
 
-    std::string path = "logs.txt";
-    manager.addSink(std::unique_ptr<FileSinkImpl>(new FileSinkImpl(path)));
-
-    LogMessage msg("App0", "CPU", 20);
-    manager.writeToAll(msg);
+    MyMomyntumSync myMomyntumSync;
+    myMomyntumSync.start();
 }
